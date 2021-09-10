62e46680f70f6aaa77858c8c72549037ed0d614a
HEL3X <person_of_paradise@hotmail.com>
2021-09-09 19:14:57 -0700

    not done, just to bring to other PC.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,20 +4,37 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int counter = 0;
+        for (int i = 0; i<str.length(); i++){
+            if (str.charAt(i) == 'a'){
+                counter = counter + 1;
+            }else if (str.charAt(i) == 'b'){
+                counter = counter + 1;
+            }else if (str.charAt(i) == 'c'){
+                counter = counter + 1;
+            }else{
+                counter = counter + 0;
+            }
+        }
+        return counter;
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
+        if (str.contains("The") || str.contains("the")){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        if(str.equals(str.substring(str.length(), 0)){
+            return true;
+        }else{
+            return false;
+        }
     }
     
     
